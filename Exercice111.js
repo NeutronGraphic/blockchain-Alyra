8e0b630121a208400531c080903f8bfef9690225
NeutronGraphic <neutrongraphic@gmail.com>
2019-04-30 12:28:48 +0200

    modification condition

--- Exercice111.js
+++ Exercice111.js
@@ -1,9 +1,9 @@
-var n;
+
 var m;
 
-n = prompt("Entrer un nombre entier entre 1 et 100");
+var n = prompt("Entrer un nombre entier entre 1 et 100");
 
-console.log(n);
+console.log(n);//affiche la valeur à devinner
 
 while(n<1 || n>100){
 	console.log("Valeur incorrècte rentrez à nouveau une valeur ");
@@ -16,19 +16,19 @@ m = prompt("Devine la réponse précédente")
 while(m!=n){
 	if(m<n){
     	console.log("Superieur");
-      	if((m-n<=5)){
+      	if(n-m<=5){
     			console.log("Très proche");	
     		}
-    		else if((m-n<=10)){
+    		else if(n-m<=10){
     			console.log("Proche");
     		}
     }
    else if(m>n){
     	console.log("inferieur");
-      if((m-n<=5)){
+      	if((m-n<=5)){
     			console.log("Très proche");	
     		}
-    		else if((m-n<=10)){
+    		else if(m-n<=10){
     			console.log("Proche");
     		}
     }
